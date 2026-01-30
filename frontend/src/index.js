fdbff6124088bd8a1c3d8212e8232bcdf36dd8d9
charanreddy9081 <123448951+charanreddy9081@users.noreply.github.com>
2026-01-30 11:34:58 +0530

    Remove BrowserRouter from index.js
    Removed BrowserRouter wrapper from the root render.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/index.css";
 import App from "@/App";
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-import { BrowserRouter } from "react-router-dom";
-
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/project">
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
